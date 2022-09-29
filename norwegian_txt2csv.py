37b18c249fb2147fb64dda4a26f9230b42ccdc38
saldejummi <micgrig@gmail.com>
2022-09-29 20:40:17 +0300

    fix negative floats

--- norwegian_txt2csv.py
+++ norwegian_txt2csv.py
@@ -1,7 +1,8 @@
+#https://github.com/saldejummi/norwegian_txt2csv
 import re
 
-FILENAME_SOURCE = '2020.txt'
-FILENAME_DESTINATION = '2020.csv'
+FILENAME_SOURCE = '2019.txt'
+FILENAME_DESTINATION = '2019.csv'
 
 with open(FILENAME_SOURCE, 'r') as source:
     with open(FILENAME_DESTINATION, 'w') as destination:
@@ -24,5 +25,10 @@ with open(FILENAME_DESTINATION, 'r') as f:
         if searchObject.group(1) == 'points':
             continue
         else:
-            points += int(float(searchObject.group(1))*100)
-    print(points/100)
+            try:
+                points += int(float(searchObject.group(1)) * 100)
+            except:
+                #points += int(float(searchObject.group(1).replace('\U00002013', '-')) * 100)
+                replacedAllNonASCCIIchars = re.sub(r'[^\x00-\x7F]+','-', searchObject.group(1))
+                points += int(float(replacedAllNonASCCIIchars) * 100)
+    print(points / 100)
